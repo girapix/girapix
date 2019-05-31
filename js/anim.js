f4cc911df70fb2abc2b9ee28b7fe51032ce78b79
razik <ellifaz.marciano@outlook.com>
2019-05-31 09:02:13 -0300

    finaliza animações

--- js/anim.js
+++ js/anim.js
@@ -9,14 +9,30 @@ $(document).ready(function () {
     $('.materialboxed').materialbox();
 });
 
+// desativar o sidenav quando clicar fora dela
 
 $('.sidenav li').click(() => {
     $('.sidenav').sidenav('close');
 });
+
+// colorir a navbar on scroll
+
 $(window).on("scroll", function(){
     if($(window).scrollTop() > 100) {
         $(".navbar").addClass("nav-color");
     } else {
         $(".navbar").removeClass("nav-color");
     }
+});
+
+// Animar botoes do site
+$(document).ready(function () {
+    var animacaoTipo = 'animated flipOutX';
+    var animacaoFim = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationEnd animationend';
+
+    $('a.btn-anim').on('click', function() {
+        $(this).addClass(animacaoTipo).one(animacaoFim, function() {
+            $(this).removeClass(animacaoTipo);
+        });
+    });
 });
